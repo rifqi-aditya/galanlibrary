757882d8e7ca7d33d6cd00ca316d23886a5d0f42
prabowo-rifqi <praboworifqi@student.gunadarma.ac.id>
2025-07-28 22:54:36 +0700

    Final attempt to fix cache path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,11 +15,12 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$app->useConfigCache('/tmp/config.php');
-$app->useRoutesCache('/tmp/routes.php');
-$app->useEventsCache('/tmp/events.php');
-$app->useServicesCache('/tmp/services.php');
-$app->usePackagesCache('/tmp/packages.php');
+if (!is_dir('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
 
 /*
 |--------------------------------------------------------------------------
